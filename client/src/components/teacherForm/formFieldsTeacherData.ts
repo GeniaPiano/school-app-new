b801fc3653aea25e610685969a4e58eb115b9da4
GeniaPiano <genia.piano@hotmail.com>
2023-09-05 10:58:24 +0200

    [FE] chaged types in add form teacher

---
+++ client/src/components/teacherForm/formFieldsTeacherData.ts
@@ -0,0 +1,24 @@
+
+export const formFieldsTeacherData = [
+    {
+        name: 'name',
+        type: 'text',
+        title: 'First name',
+        errorMessage: 'First name is required and must have at least 3 characters.',
+        minCharacters: 3,
+    },
+    {
+        name: 'last_name',
+        type: 'text',
+        title: 'Last name',
+        errorMessage: 'Last name is required and must have at least 3 characters.',
+        minCharacters: 3,
+    },
+    {
+        name: 'email',
+        type: 'email',
+        title: 'Email',
+        errorMessage: 'Email is required and must have at least 4 characters.',
+        minCharacters: 4,
+    }
+];''
