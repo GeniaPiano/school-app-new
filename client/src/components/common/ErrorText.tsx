cdb4b5ebed4da690f51bf9494964e17d95ca6c5c
GeniaPiano <genia.piano@hotmail.com>
2023-09-09 21:21:56 +0200

    [FE] Changed style of TextError

--- client/src/components/common/ErrorText.tsx
+++ client/src/components/common/ErrorText.tsx
@@ -1,4 +1,9 @@
+import {Text} from '@chakra-ui/react'
 
+interface Props {
+    text: string;
+}
 
-export const ErrorText = ({text}: string) =>
-    <p style={{ color:"red",  marginBottom: '5px'}}> {text} </p>
+export const ErrorText = (props: Props) => (
+    <Text color="red"  mB='5px' size='xs'> {props.text} </Text>
+    )
