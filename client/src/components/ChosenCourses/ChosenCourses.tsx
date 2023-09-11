af18530d75bd0ae278e9525e1ac7d02a7b78af94
GeniaPiano <genia.piano@hotmail.com>
2023-09-11 23:28:22 +0200

    [FE] added ChosenCourses

---
+++ client/src/components/ChosenCourses/ChosenCourses.tsx
@@ -0,0 +1,24 @@
+import {Box, SimpleGrid} from "@chakra-ui/react";
+import {CourseItem} from "../common/CourseItem";
+import {CourseEntity} from "../../types/course";
+
+interface Props {
+    data: CourseEntity[];
+    handleRemove: (courseId: string)=> void;
+}
+
+
+export const ChosenCourses = ({data, handleRemove} : Props) => {
+
+
+     return (
+         <SimpleGrid columns={3} spacing={4} my={5}>
+             {data.map(oneDataItem => (
+                 <Box key={oneDataItem.id} position="relative" bg="brand.800" color="white" p={3} borderRadius="10px" alignItems="center">
+                     <CourseItem name={oneDataItem.name} courseId={oneDataItem.id} handleRemove={handleRemove} />
+                 </Box> ))}
+         </SimpleGrid>
+     )
+
+
+}
