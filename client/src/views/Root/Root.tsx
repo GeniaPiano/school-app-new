b42fb6982e912e553f99ccfb997f6f4a2765264c
GeniaPiano <genia.piano@hotmail.com>
2023-09-19 12:56:07 +0200

    [FE] added Login form

--- client/src/views/Root/Root.tsx
+++ client/src/views/Root/Root.tsx
@@ -5,24 +5,81 @@ import {NotFoundView} from "../NotFoundView";
 import {News} from "../News/News";
 import {StudentsView} from "../StudentsView/StudentsView";
 import {TeachersView} from "../TeachersView/TeachersView";
+import {FormField} from "../../components/FormField/FormField";
+import {useState} from "react";
+
+
+const UnauthenticatedApp = () => {
+
+    const [inputValues, setInputValues] = useState ({
+        login: '',
+        password: ''
+    })
+    const [touchCount, setTouchCount] = useState({
+        login: 0,
+        password: 0,
+    })
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setInputValues(prev => ({
+            ...prev,
+            [name]: value,
+        }))
+        setTouchCount(prev => ({
+            ...prev,
+            [e.target.name]: prev[name] + 1
+        }))
+
+    }
+
+
+    const isErrorLogin = touchCount.login > 4 && (inputValues.login.length < 4 || inputValues.login.length > 40 || !inputValues.login.includes('@'))
+    const isErrorPassword = touchCount.password > 4 && (inputValues.password.length < 4 || inputValues.login.length > 40)
 
 
 
-export const Root =()=> {
+
     return (
-       <Router>
-        <AdminLayout>
-               <Routes>
-                    <Route exact path='/' element={<Navigate to={'/courses'} />} />
-                    <Route exact path='/courses/:courseId?' element={<CoursesView />} />
-                    <Route exact path='/students' element={<StudentsView/>} />
-                    <Route exact path='/teachers' element={<TeachersView/>} />
-                    <Route exact path='/news' element={<News/>} />
-                    <Route path='/logout' />
-                   <Route path='*' element={<NotFoundView/>} />
-                </Routes>
-        </AdminLayout>
-        </Router>
+        <form>
+            <FormField name="login"
+                       errorMessage='Login is required and must contain from 4 to 40 characters.'
+                       label="Login"
+                       value={inputValues.login}
+                       type='email'
+                       onChange={handleInputChange}
+                       error={isErrorLogin}
+            />
+            <FormField name="password"
+                       errorMessage='Password is required must contain from 4 to 40 characters.'
+                       label="password"
+                       type="password"
+                       value={inputValues.password}
+                       onChange={handleInputChange}
+                       error={isErrorPassword}
+            />
+        </form>
+    )
+}
+
+
+export const Root = () => {
+    return (
+        <UnauthenticatedApp/>
+
+       // <Router>
+       //  <AdminLayout>
+       //         <Routes>
+       //              <Route exact path='/' element={<Navigate to={'/courses'} />} />
+       //              <Route exact path='/courses/:courseId?' element={<CoursesView />} />
+       //              <Route exact path='/students' element={<StudentsView/>} />
+       //              <Route exact path='/teachers' element={<TeachersView/>} />
+       //              <Route exact path='/news' element={<News/>} />
+       //              <Route path='/logout' />
+       //             <Route path='*' element={<NotFoundView/>} />
+       //          </Routes>
+       //  </AdminLayout>
+       //  </Router>
 
     )
 }
