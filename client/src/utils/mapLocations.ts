ca6162619e2374214871eb587868099d983f8633
GeniaPiano <genia.piano@hotmail.com>
2023-09-30 22:37:47 +0200

    [FE] added data with locations

---
+++ client/src/utils/mapLocations.ts
@@ -0,0 +1,29 @@
+interface MapLocationItem {
+    city: string;
+    street: string;
+    lat: number;
+    lon: number;
+}
+
+export const mapLocations: MapLocationItem[] = [
+    {
+        city: 'Warszawa',
+        street: 'Rakowiecka 21',
+        lat: 52.2085348,
+        lon: 21.0133375
+    },
+    {
+        city: 'Warszawa',
+        street: 'Krasińskiego 21',
+        lat: 52.269982,
+        lon: 20.99392
+    },
+    {
+        city: 'Warszawa',
+        street: 'Szpotańskiego 4',
+        lat: 52.2063922,
+        lon: 21.1654753
+    }
+]
+
+
