df35d43c92b75f9b72e65cc8a84ab9be1166c03c
GeniaPiano <genia.piano@hotmail.com>
2023-10-07 21:16:18 +0200

    [BE] installation and configuration  dotenv

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,9 @@
 import * as express from "express";
-
 import * as cors from 'cors';
 import 'express-async-errors';
 import rateLimit from 'express-rate-limit'
+import * as cookieParser from "cookie-parser";
+require('dotenv').config();
 
 const limiter = rateLimit({
     windowMs: 5 * 60 * 1000,
@@ -16,24 +17,26 @@ const limiter = rateLimit({
 import {handleError} from "./utils/errors";
 import "./utils/db"; //połączenie z bazą danych
 
+//routers
 import {courseRouter} from "./routers/course";
 import {teacherRouter} from "./routers/teacher";
 import {studentRouter} from "./routers/student";
-import {authRouter} from "./routers/auth";
+import {authRouter} from "./routers/auth"
+import {adminRouter} from "./routers/admin"
+
 
 
 const app = express();
-
-
-
 const corsOptions = {
     origin: ['http://localhost:5173', 'http://localhost:3000', 'https://menager.networkmanager.pl'],
 };
 
+//middlewares
 app.use(cors(corsOptions));
 app.use(limiter)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -41,6 +44,7 @@ app.use('/course', courseRouter);
 app.use('/teacher', teacherRouter);
 app.use('/student', studentRouter);
 app.use('/auth', authRouter);
+app.use('/admin', adminRouter);
 app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
