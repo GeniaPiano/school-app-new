d28774987f0ef602f5c01045f401f22ae197d1d8
GeniaPiano <genia.piano@hotmail.com>
2023-10-09 14:48:48 +0200

    [BE] added new instances of errors

--- api/utils/errors.ts
+++ api/utils/errors.ts
@@ -4,6 +4,10 @@ export class ValidationError extends Error {}
 
 export class NotFoundError extends  Error {}
 
+export class NotAuthorizedError extends  Error {}
+
+export class ForbiddenError extends  Error {}
+
 export const handleError = (
     err: Error,
     req: Request,
@@ -18,36 +22,17 @@ export const handleError = (
     }
     if (err instanceof NotFoundError ) {
         res.status(404).json({message: err.message})
-    } else {
+    }
+    if (err instanceof ForbiddenError) {
+        res.status(403).json({message: err.message})
+    }
+    if (err instanceof NotAuthorizedError) {
+        res.status(401).json({message: err.message})
+    }else {
         res.status(500).json({message: 'Sorry, please try again later.'})
     }
-
 };
 
-//
-//
-// export class HttpError extends Error {
-//     code: number;
-//     constructor(code: number, message: string) {
-//         super(message);
-//         this.code = code;
-//     }
-// }
-//
-// export const handleError = (
-//     err: Error,
-//     req: Request,
-//     res: Response,
-//     next: NextFunction
-// ): void => {
-//     console.error(err);
-//
-//     if (err instanceof HttpError) {
-//         res.status(err.code).json({ message: err.message });
-//     } else {
-//         res.status(500).json({ message: 'Sorry, please try again later.' });
-//     }
-// };
 
 
 
