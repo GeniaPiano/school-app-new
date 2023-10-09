6093a8c63407e22e798db68472674c54f1e6d8e8
GeniaPiano <genia.piano@hotmail.com>
2023-10-09 14:47:47 +0200

    [BE] auth modified

--- api/controllers/auth.ts
+++ api/controllers/auth.ts
@@ -1,32 +1,35 @@
 import {NextFunction, Request, Response} from "express";
-import {getUserById, getUserWithRoleByEmail} from "../utils/getUser";
 import * as bcrypt from "bcryptjs";
 import * as jwt from "jsonwebtoken";
 import {userWithoutPassword} from "../utils/dataWithoutPassword";
 import {JwtPayload} from "jsonwebtoken";
 import {StudentRecord} from "../records/student.record";
-import {UserEntityLoginData} from "../types";
+import {UserEntityRegisterData} from "../types";
 import {checkMailAvailable} from "../utils/checkMailAvailable";
 import {ValidationError} from "../utils/errors";
+import {UserRecord} from "../records/user.record";
+
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { email, password} = req.body;
-        const user = await getUserWithRoleByEmail(email)
-
-        if (!user) {
+        const user = await UserRecord.getUserByEmail(email)
+                if (!user) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
         if (user) {
             const passwordMatch = await bcrypt.compare(password, user.password);
             if (passwordMatch) {
-                const token = jwt.sign({ userId: user.id }, 'easy-secret-key', { expiresIn: '1h' });
+                const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT, { expiresIn: '1h' });
 
-                res.json({
-                    user: userWithoutPassword(user),
-                    token,
-                })
+                res
+                    .cookie("access_token", token, {httpOnly: true})
+                    .status(200)
+                    .json({
+                        user: userWithoutPassword(user),
+                        token,
+                    })
             } else {
                 return res.status(401).json({ message: 'Invalid credentials' })
             }
@@ -37,20 +40,19 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
 }
 
 export const checkToken = async(req: Request, res: Response, next: NextFunction) => {
-        const token = req.header('Authorization').slice(7);
+        const token = req.header('Authorization')?.slice(7);
         if (token) {
             try {
 
-                const decoded = jwt.verify(token, 'easy-secret-key');
-
-                if (typeof decoded === 'object' && 'userId' in decoded) {
-                    const userId = (decoded as JwtPayload).userId; // Ręczne rzutowanie do JwtPayload
-                    const user = await getUserById(userId)
-
+                const decoded: JwtPayload | string = jwt.verify(token, process.env.JWT) as JwtPayload;
+                if (typeof decoded === 'object' && 'id' in decoded && 'role' in decoded) {
+                    const id = (decoded as JwtPayload).id
+                    const role = (decoded as JwtPayload).role;
+                    const user = await UserRecord.getUserById(id);
 
                     if (user) {
                         const clearedUser = userWithoutPassword(user);
-                        return res.status(200).json(clearedUser);
+                        return res.status(200).json({id, role, ...clearedUser});
                     }
                 } else {
                     console.error('Invalid token format');
@@ -68,25 +70,23 @@ export const logout = (req: Request, res: Response, next: NextFunction) => {
 }
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
-    const {email, password} = req.body as UserEntityLoginData
+    const {email, password} = req.body as UserEntityRegisterData;
     const hashedPassword = await bcrypt.hash(password, 10);
     const checkOkMail = await checkMailAvailable(email); //sprawdzanie dostępności maila
     if (!checkOkMail) {
         throw new ValidationError("Email already exists.")
     }
 
+    //@todo MAIL INFO TO USER
+
     const userData = {
         ...req.body,
-        name: null,
-        last_name: null,
         password: hashedPassword,
     } as StudentRecord;
     const user = await new StudentRecord(userData);
     await user.insert();
 
-    res.json({
-        student: userWithoutPassword(user)
-    })
+    res.status(200).json('User has been created')
 
 
 
--- api/routers/auth.ts
+++ api/routers/auth.ts
@@ -5,49 +5,14 @@ export const authRouter = Router();
 
 
 authRouter
-
     .post('/login', login)
     .get('/me', checkToken)
     .post ('/logout', logout)
     .post ('/register', register)
 
 
-// interface User {
-//     role: string;
-//
-// }
-// export const authorizeAdmin = () => {
-//     return (req: Request, res: Response, next: NextFunction) => {
-//         const userRole = req.user.role
-//
-//         if (userRole !== 'admin') {
-//             return res.status(403).json({ message: 'Access forbidden' });
-//         }
-//         next();
-//     };
-// };
 
-// export const authorizeStudent = () => {
-//     return (req: Request, res: Response, next: NextFunction) => {
-//         const userRole = req.user.role;
-//
-//         if (userRole !== 'student') {
-//             return res.status(403).json({ message: 'Access forbidden : student' });
-//         }
-//         next();
-//     };
-// };
-//
-// export const authorizeTeacher = () => {
-//     return (req:Request, res: Response, next: NextFunction) => {
-//         const userRole = req.user.role;
-//
-//         if (userRole !== 'teacher') {
-//             return res.status(403).json({ message: 'Access forbidden : teacher' });
-//         }
-//         next();
-//     };
-// };
+
 
 
 
