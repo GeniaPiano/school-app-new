6377606c83d943792fc2f1634ff9e736ea08ebcb
GeniaPiano <genia.piano@hotmail.com>
2023-10-11 21:04:52 +0200

    [FE] added hook useCoursesForOneStudent

---
+++ client/src/hooks/useCoursesForOneStudent.tsx
@@ -0,0 +1,33 @@
+
+import {useCourses} from "./useCourses";
+import {useEffect, useState} from "react";
+import {CourseEntity, CourseWithStartedDate} from "../types/course";
+
+const initialState = {
+    coursesAvailable: [],
+    coursesChosen: [],
+}
+
+type Courses = {
+    coursesAvailable: CourseEntity[];
+    coursesChosen: CourseWithStartedDate[];
+}
+
+export const useCoursesForOneStudent = (userId) => {
+    const { getCoursesForStudent } = useCourses();
+    const [courses, setCourses] = useState<Courses>(initialState);
+
+    useEffect(() => {
+        (async () => {
+            if (userId) {
+                const res = await getCoursesForStudent(userId);
+                setCourses(res);
+
+                 }
+        })();
+    }, [getCoursesForStudent, userId]);
+
+    return {
+        courses
+    };
+};
