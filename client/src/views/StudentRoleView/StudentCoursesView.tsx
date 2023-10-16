240215e28f09ed508f8dd3e85afa19947f8f6d75
GeniaPiano <genia.piano@hotmail.com>
2023-10-16 10:28:35 +0200

    [FE] removed baught courses from StudentCoursesView.tsx

--- client/src/views/StudentRoleView/StudentCoursesView.tsx
+++ client/src/views/StudentRoleView/StudentCoursesView.tsx
@@ -11,11 +11,11 @@ import {
 import {useCoursesForOneStudent} from "../../hooks/useCoursesForOneStudent";
 import {useAuth} from "../../hooks/useAuth";
 import {CoursesAvailableToBuy} from "../../components/StudentRole/CoursesAvailableToBuy";
-import {CoursesBought} from "../../components/StudentRole/CoursesBought";
+
 export const StudentCoursesView = () => {
 const {user} = useAuth();
 const {courses} = useCoursesForOneStudent(user?.id);
-const {coursesAvailable, coursesChosen} = courses
+const {coursesAvailable} = courses
     return (
         <Flex color="gray.500" h="95vh" mt="2.5vh" width="80%" flexDir="column" mb="5em" mr="4.5em">
             <Box>
@@ -28,13 +28,9 @@ const {coursesAvailable, coursesChosen} = courses
                 </Heading>
                 <Tabs  variant='enclosed' isFitted>
                     <TabList >
-                        <Tab _selected={{color: 'white', bg: 'myPink.400'}}>YOUR COURSES</Tab>
-                        <Tab _selected={{color: 'white', bg: 'brand.600'}} >BUY COURSES</Tab>
+                        <Tab _selected={{color: 'white', bg: 'brand.600'}} > NEW COURSES!</Tab>
                     </TabList>
                     <TabPanels mt={5}>
-                        <TabPanel >
-                            <CoursesBought coursesChosen={coursesChosen}/>
-                        </TabPanel>
                         <TabPanel>
                             <CoursesAvailableToBuy coursesAvailable={coursesAvailable}/>
                          </TabPanel>
