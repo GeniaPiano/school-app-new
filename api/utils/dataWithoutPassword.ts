b38a6cbd29b59dc08809b6d0988cf0d98320385f
GeniaPiano <genia.piano@hotmail.com>
2023-10-17 12:05:01 +0200

    [BE] corrected types

--- api/utils/dataWithoutPassword.ts
+++ api/utils/dataWithoutPassword.ts
@@ -1,16 +1,18 @@
 
 import {StudentEntity, TeacherEntity, AdminEntity} from "../types";
+import {StudentRecord} from "../records/student.record";
+import {TeacherRecord} from "../records/teacher.record";
+import {AdminRecord} from "../records/admin.record";
 
 
 
-
-
-export const userWithoutPassword = (userObj: StudentEntity | TeacherEntity | AdminEntity) => {
+export const userWithoutPassword = (userObj: StudentEntity | TeacherEntity | AdminEntity | StudentRecord | TeacherRecord | AdminRecord ) => {
     const {password, ...rest} = userObj
     return {
         ...rest
     }
 }
+
 
 
 
