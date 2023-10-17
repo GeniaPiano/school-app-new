5198b871d0c6319859bd1240de85b1af2ed80b83
GeniaPiano <genia.piano@hotmail.com>
2023-10-17 12:01:35 +0200

    [FE] adde new end-point from API

--- client/config/api.ts
+++ client/config/api.ts
@@ -5,5 +5,6 @@ export const STUDENT_URL: string = BASE_URL + '/student';
 export const COURSE_URL: string = BASE_URL + '/course';
 export const TEACHER_ULR: string = BASE_URL + '/teacher';
 export const AUTH_URL: string = BASE_URL + '/auth';
-export const SHOP_URL: string = BASE_URL + '/shop'
+export const SHOP_URL: string = BASE_URL + '/shop';
+export const DATO_URL: string = BASE_URL + '/dato';
 
