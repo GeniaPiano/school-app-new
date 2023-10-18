ee18aa90811b692c5135806607e659f1ac7a9969
GeniaPiano <genia.piano@hotmail.com>
2023-10-19 00:04:55 +0200

    [BE] Added helpers

---
+++ client/src/components/courseForm/imagesUrls.ts
@@ -0,0 +1,15 @@
+export const imagesUrls = [
+    'https://www.datocms-assets.com/107048/1697483793-dance-studio.png',
+    'https://www.datocms-assets.com/107048/1697483765-dance-studio-5.png',
+    'https://www.datocms-assets.com/107048/1697483770-dance-studio-4.png',
+    'https://www.datocms-assets.com/107048/1697483776-dance-studio-3.png',
+    'https://www.datocms-assets.com/107048/1697483782-dance-studio-2.png',
+    'https://www.datocms-assets.com/107048/1697483788-dance-studio-1.png',
+    'https://www.datocms-assets.com/107048/1697483870-dance-studio-8.png',
+    'https://www.datocms-assets.com/107048/1697483875-dance-studio-7.png',
+    'https://www.datocms-assets.com/107048/1697484069-dance-studio-10.png',
+    'https://www.datocms-assets.com/107048/1697484074-dance-studio-9.png',
+    'https://www.datocms-assets.com/107048/1697484138-dance-studio-12.png',
+    'https://www.datocms-assets.com/107048/1697484145-dance-studio-11.png',
+
+]
---
+++ client/src/components/courseForm/prices.ts
@@ -0,0 +1,1 @@
+export const prices=['40', '50']
