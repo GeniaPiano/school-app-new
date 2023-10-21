4c4f47a882bef9c2e3028d153f91a552b20ab85f
GeniaPiano <genia.piano@hotmail.com>
2023-10-21 21:15:18 +0200

    [FE] Added StarRate component and its types

---
+++ client/src/components/StarsRate/StarsRate.tsx
@@ -0,0 +1,38 @@
+import {HStack} from "@chakra-ui/react";
+import {AiFillStar} from "react-icons/ai";
+interface Props {
+    average: number;
+}
+
+export const StarsRate = ({average}:Props) => {
+
+    const createArray = (length) => {
+      const array = []
+      for (let i = 1; i <= length; i++) {
+            array.push(i);
+        }
+      return array
+    }
+
+    const numberOfColored = Math.floor(average)
+    const numberOfUnColored = 5 - numberOfColored
+    const colored = createArray(numberOfColored)
+    const unColored = createArray(numberOfUnColored)
+
+    if (!average) return  (
+        <HStack>
+            <AiFillStar style={{color: '#CCCCCC'}}/>
+            <AiFillStar style={{color: '#CCCCCC'}}/>
+            <AiFillStar style={{color: '#CCCCCC'}}/>
+            <AiFillStar style={{color: '#CCCCCC'}}/>
+            <AiFillStar style={{color: '#CCCCCC'}}/>
+        </HStack>
+    )
+
+     return (
+         <HStack>
+             {colored.map(one => <AiFillStar key={one} style={{color: 'teal'}} />)}
+             {unColored.map(one => <AiFillStar key={one} style={{color: '#CCCCCC'}}/>)}
+         </HStack>
+     )
+}
---
+++ client/src/types/rateCourse.ts
@@ -0,0 +1,7 @@
+export interface RateCourseEntity  {
+    id?: string;
+    course_id: string;
+    student_id:string;
+    stars: 1 | 2 | 3 | 4 | 5;
+    opinion?: string;
+}
