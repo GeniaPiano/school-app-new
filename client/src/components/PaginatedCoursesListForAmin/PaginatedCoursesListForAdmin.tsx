1adea02200876698dc96b1c1c872ca7a6323c0dd
GeniaPiano <genia.piano@hotmail.com>
2023-10-22 19:58:55 +0200

    [FE] Changed name of StarsRate to StarsRateAverage

--- client/src/components/PaginatedCoursesListForAmin/PaginatedCoursesListForAdmin.tsx
+++ client/src/components/PaginatedCoursesListForAmin/PaginatedCoursesListForAdmin.tsx
@@ -3,7 +3,7 @@ import {CourseAllDetails} from "../../types/course";
 import {PaginationGroupBtns} from "../PaginationGroupBtns/PaginationGroupBtns";
 import {usePagination} from "../../hooks/usePagination";
 import {averageRateCourse} from "../../utils/averageRateCourse";
-import {StarsRate} from "../StarsRate/StarsRate";
+import {StarsRateAverage} from "../StarsRateAverage/StarsRateAverage";
 import {useCourseInfo} from "../../providers/CourseProvider";
 
 interface Props {
@@ -43,7 +43,7 @@ export const PaginatedCoursesListForAdmin = ({ data, itemsPerPage} : Props) => {
                             <Td display={{base: 'none', md: 'table-cell'}}>{course.teacherName !== null ? course.teacherName : <Text as='i' color="orange"> not assigned </Text>}</Td>
                             <Td display={{base: 'none', md: 'table-cell'}} isNumeric>
                                  {course.rates.length !== 0 ? `${averageRateCourse(course.rates).toFixed(2)}/${course.rates.length}` : '0/0' }</Td>
-                            <Td  display={{base: 'none', md: 'table-cell'}}> <StarsRate average={averageRateCourse(course.rates)}/> </Td>
+                            <Td  display={{base: 'none', md: 'table-cell'}}> <StarsRateAverage average={averageRateCourse(course.rates)}/> </Td>
                             <Td display={{base: 'none', md: 'table-cell'}} isNumeric> {course.countStudents} </Td>
                             <Td display={{base: 'none', md: 'table-cell'}} isNumeric>{course.price.toFixed(2)} PLN</Td>
                             <Td display={{ md: 'table-cell'}} isNumeric>
