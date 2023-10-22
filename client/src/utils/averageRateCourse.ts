c60c842b1fefdcb9cca03a9836c9ad0d6c245249
GeniaPiano <genia.piano@hotmail.com>
2023-10-22 19:53:24 +0200

    [FE] Added rating averalge counter helper for courses

--- client/src/utils/averageRateCourse.ts
+++ client/src/utils/averageRateCourse.ts
@@ -1,6 +1,10 @@
 import {RateCourseEntity} from "../types/rateCourse";
 
-export const averageRateCourse = (rates: RateCourseEntity[]) => {
-    const sumOfStars = rates.reduce((prev, next) => prev + Number(next.stars), 0)
-    return sumOfStars/rates.length
+export const averageRateCourse = (rates: RateCourseEntity[]): number | string => {
+    if (rates.length !== 0) {
+        const sumOfStars = rates.reduce((prev, next) => prev + Number(next.stars), 0)
+        return sumOfStars/rates.length
+
+    } else return '0/0'
+
 }
